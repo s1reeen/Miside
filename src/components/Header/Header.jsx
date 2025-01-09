3579b1f560d70d54c0567a36e6d52c6b896343a9
s1reeen <s1reeen17@gmail.com>
2025-01-09 20:26:11 +0200

    hero add text? fix video, add animations on header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,13 +4,15 @@ import logo from "../../assets/logo.png";
 const Header = () => {
   return (
     <div className={css.header}>
-      <img className={css.logo} src={logo} alt="logo" />
+      <a href="/">
+        <img className={css.logo} src={logo} alt="logo" />
+      </a>
       <nav className={css.nav}>
         <ul className={css.list}>
           <li className={css.list_elem}>Home</li>
           <li className={css.list_elem}>Guides</li>
           <li className={css.list_elem}>Endings</li>
-          <li className={css.list_elem}>Mita</li>
+          <li className={css.list_elem}>Mitas</li>
         </ul>
       </nav>
     </div>
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,7 +1,12 @@
-import videoBg from "../../assets/trailer.mp4";
 import css from "./Hero.module.css";
+import videoBg from "../../assets/trailer.mp4";
+import { FaSteamSymbol } from "react-icons/fa";
 
 const Hero = () => {
+  const handleClick = () => {
+    window.location.href = "https://store.steampowered.com/app/2527500/MiSide/";
+  };
+
   return (
     <div className={css.hero}>
       <video
@@ -11,6 +16,18 @@ const Hero = () => {
         loop
         muted
       ></video>
+      <div className={css.heroText}>
+        <h1>MiSide - The Ultimate Psychological Horror Experience</h1>
+        <p>
+          Discover the immersive world of MiSide, a groundbreaking psychological
+          horror game that redefines the boundaries of interactive storytelling.
+          Dive into a captivating journey where reality blurs, and every
+          decision shapes your experience.
+        </p>
+        <button className={css.downloadButton} onClick={handleClick}>
+          Download MiSide <FaSteamSymbol />
+        </button>
+      </div>
     </div>
   );
 };
